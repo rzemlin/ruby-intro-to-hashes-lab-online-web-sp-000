2c3b6c0dc2aec36a1e21dc86e5bf3f38484e2f3a
Ryan Zemlin <rzemlin@gmail.com>
2021-02-21 16:14:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,11 +24,10 @@ end
 def monopoly_with_third_tier
 monopoly = {
 	  :railroads => {
-	    :pieces => 4, {
-	      :names => {}
+	    :pieces => 4,
+	    :names => {}
 	    }
 	  }
-  }
 end
 
 def monopoly_with_fourth_tier
